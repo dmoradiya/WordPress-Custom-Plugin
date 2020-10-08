8cfb7b4791e6a39a84c73ab3aff418d8f032444d
dhmec <dharmoradiya@gmail.com>
2020-10-07 19:15:58 -0600

    Initial commit

---
+++ stock-data.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:       Stock's Daily Data
+ * Plugin URI:        https://stockdata.ca
+ * Description:       A simple daily stock information.
+ * Version:           1.0
+ * Requires at least: 5
+ * Requires PHP:      7.4
+ * Author:            dharmesh
+ * Author URI:        https://dharmesh.codes
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
